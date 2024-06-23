678179542824103e850325ae9766d25addac477f
nico-robert <nicolas.robert.19000@gmail.com>
2024-06-23 10:47:59 +0200

    Add test file `all.tcl`.

---
+++ test/all.tcl
@@ -0,0 +1,21 @@
+# Test all files.
+lappend auto_path [file dirname [file dirname [file dirname [info script]]]]
+
+package require pix
+
+set files {
+    test_context.test
+    test_fonts.test
+    test_images.test
+    test_paints.test
+    test_paths.test
+}
+
+set dir [file dirname [info script]]
+
+# All files.
+foreach fc [split $files "\n"] {
+    set file [string trim $fc]
+    if {[string length $file] == 0 || [string first "#" $file] > -1} {continue}
+    source [file join $dir $file]
+}
