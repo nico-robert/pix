5746931ef629d3ee70f7e78ce1e38f73eb4bb7d2
nico-robert <nicolas.robert.19000@gmail.com>
2024-06-24 20:53:35 +0200

    Update parse svg example.

--- examples/parse_svg.tcl
+++ examples/parse_svg.tcl
@@ -1,15 +1,16 @@
 # 02-Jun-2024 : v1.0 : Initial example
 # 22-Jun-2024 : v2.0 : Rename proc 'pix::svg::toImage' by 'pix::svg::newImage'.
+# 24-Jun-2024 : v3.0 : Replace 'tiger.svg' by 'hello.svg' file.
 
 lappend auto_path [file dirname [file dirname [file dirname [info script]]]]
 
 package require pix
 
-set fp [open [file join [file dirname [info script]] data tiger.svg] r]
+set fp [open [file join [file dirname [info script]] data hello.svg] r]
 set data [read $fp]
 close $fp
 
-set svg [pix::svg::parse $data]
+set svg [pix::svg::parse $data {200 200}]
 set image [pix::svg::newImage $svg]
 
 set p [image create photo]
