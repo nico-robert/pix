74eed21174a606708150745724276f3e69742951
nico-robert <nicolas.robert.19000@gmail.com>
2024-06-25 13:02:24 +0200

    Update `surface` namespace (pix 0.2).

--- src/surface.nim
+++ src/surface.nim
@@ -2,14 +2,16 @@
 # Distributed under MIT license. Please see LICENSE for details.
 
 proc pix_draw_surface(clientData: Tcl.PClientData, interp: Tcl.PInterp, objc: cint, objv: Tcl.PPObj): cint =
-
+  # Draws object to Tk photo.
+  # 
+  # object - image or context
+  #
+  # Returns nothing.
   try:
     var img: Image
 
     if objc != 3:
-      let cmd = Tcl.GetStringFromObj(objv[0], nil)
-      let mess = "wrong # args: " & $cmd & " '<img>|<ctx>' 'photo'"
-      Tcl.SetResult(interp, mess.cstring , nil)
+      Tcl.WrongNumArgs(interp, 1, objv, "<img>|<ctx> 'Tk photo'")
       return Tcl.ERROR
 
     let arg1 = Tcl.GetStringFromObj(objv[1], nil)
@@ -29,14 +31,16 @@ proc pix_draw_surface(clientData: Tcl.PClientData, interp: Tcl.PInterp, objc: ci
       return Tcl.ERROR
 
     var pblock: Tk.PhotoImageBlock
-    var imgData = cast[ptr UncheckedArray[uint8]](img.data[0].addr)
+    var imgData  = cast[ptr UncheckedArray[uint8]](alloc(img.width * img.height * 4))
+    let imgpixie = cast[ptr UncheckedArray[uint8]](img.data[0].addr)
 
     var i = 0
     while i < (img.width * img.height * 4):
-      let a = float(imgData[i+3]) / 255.0;
-      imgData[i+0] = uint8(float(imgData[i + 0]) / a + 0.5)
-      imgData[i+1] = uint8(float(imgData[i + 1]) / a + 0.5)
-      imgData[i+2] = uint8(float(imgData[i + 2]) / a + 0.5)
+      let a = float(imgpixie[i+3]) / 255.0;
+      imgData[i+0] = uint8(float(imgpixie[i+0]) / a + 0.5)
+      imgData[i+1] = uint8(float(imgpixie[i+1]) / a + 0.5)
+      imgData[i+2] = uint8(float(imgpixie[i+2]) / a + 0.5)
+      imgData[i+3] = imgpixie[i+3]
       inc(i, 4)
 
     pblock.pixelPtr  = imgData
@@ -49,10 +53,13 @@ proc pix_draw_surface(clientData: Tcl.PClientData, interp: Tcl.PInterp, objc: ci
     if Tk.PhotoSetSize(interp, source, pblock.width, pblock.height) != Tcl.OK:
       return Tcl.ERROR
 
-    if Tk.PhotoPutBlock(interp, source, pblock.addr, 0, 0, pblock.width, pblock.height, 1) != Tcl.OK:
+    if Tk.PhotoPutBlock(interp, source, pblock.addr, 0, 0, pblock.width, pblock.height, Tk.PHOTO_COMPOSITE_SET) != Tcl.OK:
         return Tcl.ERROR
+    
+    if imgData != nil:
+      dealloc(imgData)
 
     return Tcl.OK
   except Exception as e:
-    echo "pix(error): ", e.msg
+    Tcl.SetResult(interp, cstring("pix(error): " & e.msg), nil)
     return Tcl.ERROR
