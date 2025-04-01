1c76d3feeb8557446ba919fd0fb753770f031e04
nico-robert <nicolas.robert.19000@gmail.com>
2025-04-01 17:45:49 +0200

    Use new `matrix` procedures instead of plain list.

--- examples/tiger.tcl
+++ examples/tiger.tcl
@@ -3,6 +3,7 @@
 
 # 02-Jun-2024 : v1.0 : Initial example
 # 22-Jun-2024 : v2.0 : rename proc 'pix::img::read' by 'pix::img::readImage'.
+# 01-Apr-2025 : v3.0 : Use new `matrix` procedures instead of plain list.
 
 lappend auto_path [file dirname [file dirname [file dirname [info script]]]]
 
@@ -13,7 +14,11 @@ pix::img::fill $image "rgba(255, 255, 255, 1)"
 
 
 set tiger [pix::img::readImage [file join [file dirname [info script]] data tiger.svg]]
-pix::img::draw $image $tiger {0.2 0 0 0 0.2 0 10 10 1}
+pix::img::draw $image $tiger [pix::mulMatrix \
+    [pix::transMatrix {100 100}] \
+    [pix::scaleMatrix {0.2 0.2}] \
+    [pix::transMatrix {-450 -450}] \
+]
 
 set p [image create photo]
 pix::drawSurface $image $p
