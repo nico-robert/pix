f316f71950ee3b46f11b48e2e4facc03e8f353ae
nico-robert <nicolas.robert.19000@gmail.com>
2025-09-25 21:31:46 +0200

    Cosmetic changes.

--- pkgIndex.tcl
+++ pkgIndex.tcl
@@ -2,9 +2,8 @@
 # Distributed under MIT license. Please see LICENSE for details.
 
 package ifneeded pix 0.6 [list apply {dir {
-    # REMINDER ME : Change the 2 values for the version.
     package require platform
-    
+
     # Check Tcl version.
     set version [expr {
         [package vsatisfies [package provide Tcl] 9.0-] ? 9 : 8
@@ -15,8 +14,8 @@ package ifneeded pix 0.6 [list apply {dir {
     set pixv 0.6
 
     switch -exact $os {
-        macosx-x86_64 - 
-        macosx-arm    - 
+        macosx-x86_64 -
+        macosx-arm    -
         linux-x86_64 {set lib [format {lib%spix%s%s} $version $pixv $ext]}
         win32-x86_64 {set lib [format {pix%s-%s%s} $version $pixv $ext]}
         default      {error "'$os' not supported for 'pix' package."}
