06bd98c7be7a80a5e61fd67aa53a5760d9f2c719
nico-robert <nicolas.robert.19000@gmail.com>
2025-11-17 17:52:18 +0100

    Adds `resvg` to svg procedures if resvg is compiled in.

--- src/svg.nim
+++ src/svg.nim
@@ -6,42 +6,117 @@ proc pix_svg_parse(clientData: Tcl.TClientData, interp: Tcl.PInterp, objc: cint,
   #
   # svg  - string data
   # size - list width,height (optional:SVGviewbox)
+  # opts - dict options (if compiled with **resvg** lib) (optional)
+  #
+  #  **resvg** dictionary options. The options are:<br>
+  #  #Begintable
+  #  **dpi**                : Sets the target DPI.
+  #  **mtx**                : The transformation matrix (*matrix3x3*) to apply to the SVG.
+  #  **styleSheet**         : The style sheet to use.
+  #  **serifFontFamily**    : Sets the `serif` font family.
+  #  **sansSerifFontFamily**: Sets the `sans-serif` font family.
+  #  **cursiveFontFamily**  : Sets the `cursive` font family.
+  #  **fantasyFontFamily**  : Sets the `fantasy` font family.
+  #  **monospaceFontFamily**: Sets the `monospace` font family.
+  #  **languages**          : Sets the list of languages.
+  #  **shapeRenderingMode** : Sets the default shape rendering method.
+  #  **textRenderingMode**  : Sets the default text rendering method.
+  #  **imageRenderingMode** : Sets the default image rendering method.
+  #  **fontFamily**         : Sets the default font family.
+  #  **loadFontFile**       : Loads a font file into the internal fonts database.
+  #  **fontSize**           : Sets the default font size. 
+  #  **loadSystemFonts**    : Loads system fonts into the internal fonts database.
+  #  #EndTable
   #
   # Returns: A *new* [svg] object.
-  if objc notin [2, 3]:
-    Tcl.WrongNumArgs(interp, 1, objv,
-      "'svg string' ?{width height}:optional"
-    )
-    return Tcl.ERROR
-
-  let ptable = cast[PixTable](clientData)
-
-  # Svg string
-  let arg1 = $objv[1]
-  var svg: Svg
-
-  if objc == 3:
-    var width, height: cint
-
-    # Size
-    if pixParses.getListInt(interp, objv[2], width, height, 
-      "wrong # args: 'size' should be 'width' 'height'") != Tcl.OK:
+  when defined(resvg):
+    if objc notin (2..4):
+      Tcl.WrongNumArgs(interp, 1, objv,
+        "'svg string' ?{width height} ?opts"
+      )
       return Tcl.ERROR
 
-    try:
-       svg = parseSvg(arg1, width, height)
-    except PixieError as e:
-      return pixUtils.errorMSG(interp, "pix(error): " & e.msg)
+    let ptable = cast[PixTable](clientData)
+
+    # Svg string
+    let arg1 = $objv[1]
+    var svg: Resvg
+
+    if objc == 4:
+      var width, height: cint
+
+      # Size
+      if pixParses.getListInt(interp, objv[2], width, height, 
+        "wrong # args: 'size' should be 'width' 'height'") != Tcl.OK:
+        return Tcl.ERROR
+
+      var opts = RenderResvgOpts()
+      try:
+        resvg.options(interp, objv[3], opts)
+        svg = resvg.parse(arg1, width, height, some(opts))
+      except ValueError as e:
+        return pixUtils.errorMSG(interp, "pix(error): " & e.msg)
+
+    elif objc == 3:
+      var width, height: cint
+
+      # Size
+      if pixParses.getListInt(interp, objv[2], width, height, 
+        "wrong # args: 'size' should be 'width' 'height'") == Tcl.OK:
+        svg = resvg.parse(arg1, width, height)
+      else:
+        var opts = RenderResvgOpts()
+        try:
+          resvg.options(interp, objv[3], opts)
+          svg = resvg.parse(arg1, option = some(opts))
+        except ValueError as e:
+          return pixUtils.errorMSG(interp, "pix(error): " & e.msg)
+    else:
+      # pixie SVG
+      try:
+        svg = resvg.parse(arg1)
+      except ValueError as e:
+        return pixUtils.errorMSG(interp, "pix(error): " & e.msg)
+
+    let p = toHexPtr(svg)
+    ptable.addRESVG(p, svg)
+
+    Tcl.SetObjResult(interp, Tcl.NewStringObj(p.cstring, -1))
   else:
-    try:
-       svg = parseSvg(arg1)
-    except PixieError as e:
-      return pixUtils.errorMSG(interp, "pix(error): " & e.msg)
+    if objc notin [2, 3]:
+      Tcl.WrongNumArgs(interp, 1, objv,
+        "'svg string' ?{width height}:optional"
+      )
+      return Tcl.ERROR
 
-  let p = toHexPtr(svg)
-  ptable.addSVG(p, svg)
+    let ptable = cast[PixTable](clientData)
 
-  Tcl.SetObjResult(interp, Tcl.NewStringObj(p.cstring, -1))
+    # Svg string
+    let arg1 = $objv[1]
+    var svg: Svg
+
+    if objc == 3:
+      var width, height: cint
+
+      # Size
+      if pixParses.getListInt(interp, objv[2], width, height, 
+        "wrong # args: 'size' should be 'width' 'height'") != Tcl.OK:
+        return Tcl.ERROR
+
+      try:
+        svg = parseSvg(arg1, width, height)
+      except PixieError as e:
+        return pixUtils.errorMSG(interp, "pix(error): " & e.msg)
+    else:
+      try:
+        svg = parseSvg(arg1)
+      except PixieError as e:
+        return pixUtils.errorMSG(interp, "pix(error): " & e.msg)
+
+    let p = toHexPtr(svg)
+    ptable.addSVG(p, svg)
+
+    Tcl.SetObjResult(interp, Tcl.NewStringObj(p.cstring, -1))
 
   return Tcl.OK
 
@@ -52,18 +127,29 @@ proc pix_svg_newImage(clientData: Tcl.TClientData, interp: Tcl.PInterp, objc: ci
   #
   # Returns: A *new* [img] object.
   if objc != 2:
-    Tcl.WrongNumArgs(interp, 1, objv, "<svg>")
+    let text = if defined(resvg): "<resvg>" else: "<svg>"
+    Tcl.WrongNumArgs(interp, 1, objv, text.cstring)
     return Tcl.ERROR
 
   # Svg
   let ptable = cast[PixTable](clientData)
-  let svg = ptable.loadSVG(interp, objv[1])
-  if svg.isNil: return Tcl.ERROR
 
-  let img = try:
-    newImage(svg)
-  except PixieError as e:
-    return pixUtils.errorMSG(interp, "pix(error): " & e.msg)
+  # Image
+  let img =
+    when defined(resvg):
+      let svg = ptable.loadRESVG(interp, objv[1])
+      if svg.isNil: return Tcl.ERROR
+      try:
+        resvg.toImage(svg)
+      except Exception as e:
+        return pixUtils.errorMSG(interp, "pix(error): " & e.msg)
+    else:
+      let svg = ptable.loadSVG(interp, objv[1])
+      if svg.isNil: return Tcl.ERROR
+      try:
+        newImage(svg)
+      except PixieError as e:
+        return pixUtils.errorMSG(interp, "pix(error): " & e.msg)
 
   let p = toHexPtr(img)
   ptable.addImage(p, img)
@@ -79,7 +165,8 @@ proc pix_svg_destroy(clientData: Tcl.TClientData, interp: Tcl.PInterp, objc: cin
   #
   # Returns: Nothing.
   if objc != 2:
-    Tcl.WrongNumArgs(interp, 1, objv, "<svg>|string('all')")
+    let text = if defined(resvg): "<resvg>|string('all')" else: "<svg>|string('all')"
+    Tcl.WrongNumArgs(interp, 1, objv, text.cstring)
     return Tcl.ERROR
 
   let ptable = cast[PixTable](clientData)
@@ -87,8 +174,14 @@ proc pix_svg_destroy(clientData: Tcl.TClientData, interp: Tcl.PInterp, objc: cin
 
   # Svg
   if key == "all":
-    ptable.clearSVG()
+    when defined(resvg):
+      ptable.clearRESVG() 
+    else:
+      ptable.clearSVG()
   else:
-    ptable.delKeySVG(key)
+    when defined(resvg):
+      ptable.delKeyRESVG(key)
+    else:
+      ptable.delKeySVG(key)
 
   return Tcl.OK
